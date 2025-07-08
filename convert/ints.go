370550ec53786ddd6f5f4e14725492fe7bf18448
Wang Jun Rui <a0970606353@icloud.com>
2025-07-08 08:06:26 +0800

    [*]added convery

--- convert/ints.go
+++ convert/ints.go
@@ -5,3 +5,7 @@ import "strconv"
 func FromInt64ToString(n int64) string {
 	return strconv.FormatInt(n, 10)
 }
+
+func FromUintToString(n uint) string {
+	return strconv.FormatUint(uint64(n), 10)
+}
